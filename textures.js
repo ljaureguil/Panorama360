bd7309c2f761957760acc8417851a187307ee801
ljaureguil <33967760+ljaureguil@users.noreply.github.com>
2022-03-12 08:33:22 -0800

    Update textures.js

--- textures.js
+++ textures.js
@@ -1,12 +1,39 @@
-var pic1="https://i.ytimg.com/vi/B6EKlcW2Jp0/maxresdefault.jpg";
-var pic2="https://cdn-az.allevents.in/events5/banners/1b24a83bd2a7c621c8cf41b53f0a9250a7aca511754675d9c74ae0c8b9b16df0-rimg-w1200-h600-gmir.jpg?v=1645658560";
-var pic3="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtf3Rvj9wjWegYCQxs7ZcnaMLjAiX1Xp_SF1wTsN5PF9LoNmbL04_ZKxnRKz3AVG_UVAg&usqp=CAU";
-var pic4="https://i.ytimg.com/vi/QlQw8gswI5A/maxresdefault.jpg";
-var pic5="https://cdn.vor.us/event/470011/og/0a25fed6290844ae920a263475c2f970.image!jpeg.168828.jpg.xalos1.jpg"
-var w1='https://as2.ftcdn.net/v2/jpg/03/37/62/41/1000_F_337624179_SCcRL4gleCZGWyVNS8suSUvyEwBoI1hb.jpg'
-var w2='https://ak.picdn.net/shutterstock/videos/1068260144/thumb/1.jpg?ip=x480'
-var w3='https://c4.wallpaperflare.com/wallpaper/189/752/873/landscape-photography-of-twigs-during-golden-hour-wallpaper-preview.jpg'
-var w4='https://t3.ftcdn.net/jpg/01/81/61/50/360_F_181615003_j5vvF46SlLp3kflyknHqh0RP7nNBw5oW.jpg'
-var w5='https://l13.alamy.com/360/T3GP16/full-seamless-spherical-panorama-360-by-180-degrees-angle-view-on-the-shore-of-small-river-with-bridge-in-city-park-in-summer-day-in-equirectangular-p-T3GP16.jpg'
-var arimgs=[];
+
 var worlds=[];
+var art=[]
+art.push({"name":"logo","pic":"https://as2.ftcdn.net/v2/jpg/03/37/62/41/1000_F_337624179_SCcRL4gleCZGWyVNS8suSUvyEwBoI1hb.jpg"});
+art.push({"name":"logo1","pic":"https://i.ytimg.com/vi/B6EKlcW2Jp0/maxresdefault.jpg"});
+art.push({"name":"logo2","pic":"https://c4.wallpaperflare.com/wallpaper/189/752/873/landscape-photography-of-twigs-during-golden-hour-wallpaper-preview.jpg"});
+art.push({"name":"logo3","pic":"https://t3.ftcdn.net/jpg/01/81/61/50/360_F_181615003_j5vvF46SlLp3kflyknHqh0RP7nNBw5oW.jpg"});
+art.push({"name":"logo4","pic":"https://ak.picdn.net/shutterstock/videos/1068260144/thumb/1.jpg?ip=x480"});
+art.push({"name":"logo5","pic":"https://l13.alamy.com/360/T3GP16/full-seamless-spherical-panorama-360-by-180-degrees-angle-view-on-the-shore-of-small-river-with-bridge-in-city-park-in-summer-day-in-equirectangular-p-T3GP16.jpg"});
+ 
+var te_=THREE.ImageUtils.loadTexture(art[1].pic);
+var ma_=new THREE.MeshPhongMaterial({map:te_});
+
+
+function Box(name,w,l,h,c){
+    var m=new THREE.Mesh(new THREE.BoxBufferGeometry( w,l,h),ma_);
+    m.name=name
+    return  m;
+}
+function Sphere(name,r,sx,sy,c){ 
+    var m=new THREE.Mesh(new THREE.SphereBufferGeometry(r, sx, sy), ma_);
+    m.name=name
+    return  m;
+}
+function Cylinder(name,r1,r2,h,s,cap,c){
+    if(cap===undefined)cap=false;
+  //  alert(n+" r1,r2,h,s,cap\n\n"+r1+"\n"+r2+"\n"+h+"\n"+s+"\n"+cap+"\n")
+  // var g=new CylinderGeometry({radiusTop : Float, radiusBottom : Float, height : Float, radialSegments : Integer, heightSegments : Integer, openEnded : Boolean, thetaStart : Float, thetaLength : Float})
+    var m= new THREE.Mesh(new THREE.CylinderGeometry( r1, r2, h, s), ma_);//,starc,arc)/// new THREE.CylinderGeometry( 5, 5, 20, 32 )/
+ m.name=name;
+
+ m.geometry.openEnded=cap;
+ return  m;
+}
+function Torus(name,r,tk,rs,ts,arc,c){
+    var m= new THREE.Mesh( geometry = new THREE.TorusGeometry( r, tk, rs, ts,arc ), ma_);
+ m.name=n
+ return  m;
+}
